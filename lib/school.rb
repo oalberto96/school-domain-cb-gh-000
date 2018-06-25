e77bdfb12b3c58cddff3f0882e2406b48599b7ec
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-25 05:28:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
+require 'pry'
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grades_students)
+    @roster[grades_students]
+  end
+
+  def sort
+    if @roster
+      @roster.collect do |grade, students|
+        students.sort!
+      end
+    end
+    @roster
+  end
+end
